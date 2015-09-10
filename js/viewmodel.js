fc4db8bd0d9a884478e5e6b24852de6521c54b8c
Tony Alongi <aalongi2@gmail.com>
2015-09-09 23:21:14 -0400

    Implemented Foursquare data into infowindows
    Makes call to Foursquare API using hardcoded venue id to return data
    about a location. Data is then set to the appropriate infoWindow

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -8,27 +8,32 @@ function Model() {
 		name: "Earl's Beer + Cheese",
 		lat: 40.7873751,
 		lng: -73.9516333,
-		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-275-beer.png'
+		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-275-beer.png',
+		venue_id: '4d1e8f8e5acaa35dfb7eb835'
 	},{
 		name: "Dough Loco",
 		lat: 40.787898,
 		lng: -73.9540978,
-		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-273-cake.png'
+		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-273-cake.png',
+		venue_id: '522b252c11d2e497993b82c3'
 	},{
 		name: "Cooper Hewitt Smithsonian Design Museum",
 		lat: 40.7843958,
 		lng: -73.9578732,
-		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-90-building.png'
+		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-90-building.png',
+		venue_id: '4a2fc4d3f964a520da981fe3'
 	},{
 		name: "Conservatory Garden",
 		lat: 40.793778,
 		lng:  -73.952454,
-		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-311-flower.png'
+		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-311-flower.png',
+		venue_id: '4d2b4592d86aa0907fa322c0'
 	},{
 		name: "ABV",
 		lat: 40.786996,
 		lng:  -73.950662,
-		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-277-cutlery.png'
+		icon: 'lib/glyphicons_free/glyphicons/png/glyphicons-277-cutlery.png',
+		venue_id: '4f0f2da5e4b01660de447b3b'
 	}
 	];
 
@@ -37,6 +42,8 @@ function Model() {
 
 	//Create an empty array to store a list of map markers
 	self.markers = [];
+
+	self.infoWindows = [];
 
 
 }
@@ -47,11 +54,19 @@ function ViewModel() {
 
     var self = this;
 
+    //Variables to hold Foursquare API tokens
+    var CLIENT_ID = "23ZBVKL12XL44XDMPUJZFHNY2ZHSQTNGCMOAFJ0HTHC1EG3S";
+    var CLIENT_SECRET = "E3PEPIVQ4TAE00V5CGRGB3PEO2CS5TMH4YM14EKJN4L5BALN";
+
     //Set variable to track which map marker is currently selected
 	var markerBouncing = null;
 
 	//Set variable to track which infowindow is currently open
 	var openInfoWindow = null;
+
+	//Declare array for storing map marker content strings from FourSquare data
+	var HTMLcontentString = '';
+	self.contentStrings = [];
 
     /* Define observables here */
 
@@ -76,8 +91,10 @@ function ViewModel() {
 	    self.results = ko.observableArray(self.initResultsList.slice(0));
 	}
 
+	//Initialize the list with hard-coded locations
 	self.initResults(MODEL.locations);
 
+	//Checks search query against all locations and filters the list and map markers if query is matched
 	self.updateListAndMap = function() {
 		if (self.searchList.indexOf(self.searchTerm().toLowerCase()) > -1) {
 			self.results.removeAll();
@@ -96,6 +113,14 @@ function ViewModel() {
 			});
 		};
 	}.bind(this);
+
+	//Function to clear the search filter input box
+	self.clearSearch = function() {
+		self.searchTerm('');
+		if (openInfoWindow) openInfoWindow.close();
+		if (markerBouncing) markerBouncing.setAnimation(null);
+		self.updateListAndMap();
+	}
 
     /* Define and use Google Map objects here */
 
@@ -141,6 +166,7 @@ function ViewModel() {
 	  };
 
 	  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+	  MODEL.infoWindows.push(infoWindow);
 
 	  google.maps.event.addListener(marker, "click", function() {
 	    if (openInfoWindow) openInfoWindow.close();
@@ -195,21 +221,38 @@ function ViewModel() {
 	  }
 	}
 
-	//Initialize the map with a list of locations hardcoded in data model
-	self.initMap(MODEL.locations);
+	//Handle JSONP request
+	self.getLocationData = function(locations) {
+	  for (var i=0; i<locations.length; i++) {
+		  var url = "https://api.foursquare.com/v2/venues/"+locations[i].venue_id+"?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20150909&callback=ViewModel.callback";
+		  var newScriptElement = document.createElement("script");
+		  newScriptElement.setAttribute("src", url);
+		  newScriptElement.setAttribute("id", "jsonp");
+		  var oldScriptElement = document.getElementById("jsonp");
+		  var head = document.getElementsByTagName("head")[0];
+		  if (oldScriptElement == null) {
+		    head.appendChild(newScriptElement);
+		  } else {
+		    head.replaceChild(newScriptElement, oldScriptElement);
+		  }
+	  };
 
-	//Function to clear the search filter input box
-	self.clearSearch = function() {
-		self.searchTerm('');
-		if (openInfoWindow) openInfoWindow.close();
-		if (markerBouncing) markerBouncing.setAnimation(null);
-		self.updateListAndMap();
 	}
+
+	self.callback = function(data) {
+	  	HTMLcontentString = "<h3>"+data.response.venue.name+"</h3>"+"<p>Rating: <strong>"+data.response.venue.rating+"</strong></p><p>Tags: <em>"+data.response.venue.tags+"</em></p>";
+	  	self.contentStrings.push(HTMLcontentString);
+	  	MODEL.infoWindows[self.contentStrings.length - 1].setContent(HTMLcontentString);
+	}
+
+	//Make request to get FourSquare data
+	self.getLocationData(MODEL.locations);
+
+	//Initialize the map with a list of locations hardcoded in data model and foursquare data for marker window content
+	self.initMap(MODEL.locations);
 
 }
 
 var ViewModel = new ViewModel();
-
-
 
 ko.applyBindings(ViewModel);
